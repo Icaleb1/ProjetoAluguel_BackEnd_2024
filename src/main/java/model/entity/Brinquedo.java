fea91f02a0a653b1e2c07406bb243d64c95cb6e9
Icaleb1 <calebfernandes080@gamil.com>
2024-05-30 23:45:15 -0300

    Criação do repository de Brinquedos

--- src/main/java/model/entity/Brinquedo.java
+++ src/main/java/model/entity/Brinquedo.java
@@ -14,7 +14,6 @@ public class Brinquedo {
 		// TODO Auto-generated constructor stub
 	}
 
-
 	public Brinquedo(int id, String nome, String descricao, int quantEstoque, double valorDiaria) {
 		super();
 		this.id = id;
@@ -24,51 +23,41 @@ public class Brinquedo {
 		this.valorDiaria = valorDiaria;
 	}
 
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getNome() {
 		return nome;
 	}
-
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
 	public String getDescricao() {
 		return descricao;
 	}
-
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
-
 	public int getQuantEstoque() {
 		return quantEstoque;
 	}
-
 
 	public void setQuantEstoque(int quantEstoque) {
 		this.quantEstoque = quantEstoque;
 	}
 
-
 	public double getValorDiaria() {
 		return valorDiaria;
 	}
-
 
 	public void setValorDiaria(double valorDiaria) {
 		this.valorDiaria = valorDiaria;
