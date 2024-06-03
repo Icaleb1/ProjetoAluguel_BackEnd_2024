aab8db7dbd80fd183e851ee78f31ce49f4fda475
Icaleb1 <calebfernandes080@gamil.com>
2024-06-03 09:00:05 -0300

    atualização do controller de endereço

--- src/main/java/controller/EnderecoController.java
+++ src/main/java/controller/EnderecoController.java
@@ -1,8 +1,14 @@
 package controller;
 
+import java.util.List;
+
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import model.entity.Endereco;
@@ -18,5 +24,35 @@ public class EnderecoController {
 	public Endereco salvar(Endereco novoEndereco) {
 		return enderecoService.salvar(novoEndereco);
 	}
+	
+
+	@DELETE 
+	@Path("/{id}")
+	public boolean excluir(@PathParam("id") int id) {
+		return enderecoService.exluir(id);
+	}
+	
+	@PUT
+	@Path("/atualizar")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public boolean alterar(Endereco enderecoAlterado){
+		return enderecoService.alterar(enderecoAlterado);
+	}
+	
+	@GET
+	@Path("/{id}")
+	public Endereco consultarPorId(@PathParam("id") int id){
+		 return enderecoService.consultarPorId(id);
+	}
+	
+	@GET
+	@Path("/todas")
+	public List<Endereco> consultarTodas(){
+		return enderecoService.consultarTodos();
+	}
+
+	
+
 
 }
